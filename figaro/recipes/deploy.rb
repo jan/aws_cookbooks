e3c31c0d011335c9ec643730d21d26f6475d7e3b
Jan Bromberger <jan.bromberger@gmx.de>
2016-01-10 11:00:24 +0100

    Link file

--- figaro/recipes/deploy.rb
+++ figaro/recipes/deploy.rb
@@ -10,10 +10,16 @@ node[:deploy].each do |application, deploy|
     deploy[:environment]["RAILS_ENV"] => deploy[:environment_variables].to_hash
   }
   
-  file ::File.join(deploy[:deploy_to], 'shared', 'config', 'application.yml') do
+  shared_file_name = ::File.join(deploy[:deploy_to], 'shared', 'config', 'application.yml')
+  file shared_file_name do
     mode '0600'
     owner deploy[:user]
     group deploy[:group]
     content YAML::dump(content)
+  end
+  
+  link_name = ::File.join(deploy[:current_path], 'config', 'application.yml')
+  link shared_file_name do
+    to link_name
   end
 end
